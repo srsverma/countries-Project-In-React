f985f31c00a99371b09ce9b883f0cb5394e02623
srsverma <skv7615@gmail.com>
2025-08-07 16:45:37 +0530

    Fix: Case-sensitive import for Header component

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 
 
-import Header from "./components/header"
+import Header from "./components/Header"
 import "./App.css"
 import { Outlet } from "react-router"
 import { ThemeProvider } from "./contexts/ThemeContext"
